884bc454520e65c6436cf2da6bd99d27077f8d27
RUTHZILBER <79808312+RUTHZILBER@users.noreply.github.com>
2021-02-28 22:12:20 +0200

    Add files via upload

---
+++ AnalyzeExample/FinalTag.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnalyzeExample
+{
+    /// <summary>
+    /// תגית משיר
+    /// </summary>
+    class FinalTag
+    {
+        public FinalTag(string fullTag, List<Tagit> informTags)
+        {
+            FullTag = fullTag;
+            InformTags = informTags;
+        }
+        /// <summary>
+        /// התגית (יתכנו כמה מילים
+        /// </summary>
+        public string FullTag { get; set; }
+        /// <summary>
+        /// רשימת תגים המכילים עבור כל מילה בתגית את הנתוח והמילה עצמה
+        /// </summary>
+        public List<Tagit> InformTags{ get; set; }
+    }
+}
